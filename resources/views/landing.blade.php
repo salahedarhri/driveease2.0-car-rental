b0de9a124902c4495d6ddf6790a35828614f306a
salahedarhri <arhri.salah@gmail.com>
2024-02-16 17:43:59 +0100

    updated resume display + update on svgs

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,64 +2,73 @@
 
 @section('content')
 
-    <!-- First Part -->
-    <div class="w-full bg-slate-200">
+<!-- First Part -->
+<div class="w-full bg-slate-200">
 
-      <div class="max-w-7xl mx-auto pb-4">
-        <div class="font-montserrat grid md:grid-cols-2 max-md:grid-cols-1 gap-0 max-w-7xl mx-auto justify-center items-center">
-          <div class="px-4 md:pt-12 max-md:pt-6 max-md:text-center">
-            <p class="text-sm px-2 text-teal-500">____Bienvenue chez DriveEase</p>
-            <p class="md:text-4xl max-md:text-2xl py-3 text-sky-800 font-bold">Trouvez la voiture qui vous correspond</p>
-            <p class="text-md text-sky-950 pb-3">&ensp;Que vous planifiez une escapade en ville, une aventure sur la route ou un voyage d'affaires, notre processus de réservation convivial 
-              vous permet de trouver et de réserver la voiture parfaite en quelques clics.
-            </p>
-          </div>
-
-          <div class="svgBackground px-4 max-md:hidden">
-            <img src="{{ asset('images/composants/landing.png') }}" alt="landing car photo" class="aspect-auto my-auto">
-          </div>
-
-        </div>
-
-        <!-- Formulaire -->
-        @include('composants.landingFormulaire')
-
+  <div class="max-w-7xl mx-auto pb-8">
+    <div
+      class="font-montserrat grid md:grid-cols-2 max-md:grid-cols-1 gap-0 max-w-7xl mx-auto justify-center items-center">
+      <div class="px-4 md:pt-12 max-md:pt-6 max-md:text-center">
+        <p class="text-sm px-2 text-teal-500">____Bienvenue chez DriveEase</p>
+        <p class="md:text-4xl max-md:text-2xl py-3 text-sky-800 font-bold">Trouvez la voiture qui vous correspond</p>
+        <p class="text-md text-sky-950 pb-3">&ensp;Que vous planifiez une escapade en ville, une aventure sur la route
+          ou un voyage d'affaires, notre processus de réservation convivial
+          vous permet de trouver et de réserver la voiture parfaite en quelques clics.
+        </p>
       </div>
+
+      <div class="svgBackground px-4 max-md:hidden">
+        <img src="{{ asset('images/composants/landing.png') }}" alt="landing car photo" class="aspect-auto my-auto">
+      </div>
+
     </div>
 
-    <!-- Carousel -->
-    @include('composants.landingCarousel')
-    
-    <!-- Newsletter -->
-    @include('composants.landingNewsletter')
+    <!-- Formulaire -->
+    @include('composants.landingFormulaire')
 
-    <!-- fourth part -->
-    <div class="w-full bg-slate-200">
-      <div class="font-montserrat grid md:grid-cols-3 max-md:grid-cols-1 justify-center max-w-7xl mx-auto md:p-10 max-md:p-3 text-white md:gap-8 max-md:gap-3">
+  </div>
+</div>
 
-        <!-- Calendar -->
-        <div class="md:w-full max-md:w-80 text-center px-6 max-md:py-3 md:py-6 mx-auto bg-gradient-to-r from-teal-500 to-sky-500 rounded-xl shadow">
-          <i class="ri-calendar-line text-2xl"></i>
-          <p class="text-lg font-semibold py-2">Choisissez la date</p>
-          <p class="lg:text-md max-lg:text-sm py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Condimentum vitae sapien pellentesque habitant morbi tristique senectus.</p>
-        </div>
-        <!-- Car -->
-        <div class="md:w-full max-md:w-80 text-center px-6 max-md:py-3 md:py-6 mx-auto bg-gradient-to-r from-teal-500 to-sky-500 rounded-xl shadow">
-          <i class="ri-roadster-line text-2xl"></i>
-          <p class="text-lg font-semibold md:py-2 max-md:py-1">Désignez votre voiture</p>
-          <p class="mlgtext-md max-lg:text-sm md:py-3 max-md:py-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Condimentum vitae sapien pellentesque habitant morbi tristique senectus.</p>
-        </div>
-        <!-- Payment -->
-        <div class="md:w-full max-md:w-80 text-center px-6 max-md:py-3 md:py-6 mx-auto bg-gradient-to-r from-teal-500 to-sky-500 rounded-xl shadow">
-          <i class="ri-bank-card-line text-2xl"></i>
-          <p class="text-lg font-semibold py-2">Moyens diverses de payement</p>
-          <p class="lg:text-md max-lg:text-sm py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Condimentum vitae sapien pellentesque habitant morbi tristique senectus.</p>
-        </div>                
-      </div>
+{{-- Carousel
+@include('composants.landingCarousel') --}}
+
+<!-- Newsletter -->
+@include('composants.landingNewsletter')
+
+<!-- fourth part -->
+<div class="w-full bg-slate-200">
+  <div
+    class="font-montserrat grid md:grid-cols-3 max-md:grid-cols-1 justify-center max-w-7xl mx-auto md:p-10 max-md:p-6 text-white md:gap-8 max-md:gap-3">
+
+    <!-- Calendar -->
+    <div
+      class="md:w-full max-md:w-80 text-center px-6 max-md:py-3 md:py-6 mx-auto bg-gradient-to-r from-teal-500 to-sky-500 rounded-xl shadow">
+      <i class="ri-calendar-line text-2xl"></i>
+      <p class="text-lg font-semibold py-2">Choisissez la date</p>
+      <p class="lg:text-md max-lg:text-sm py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+        Condimentum vitae sapien pellentesque habitant morbi tristique senectus.</p>
     </div>
+    <!-- Car -->
+    <div
+      class="md:w-full max-md:w-80 text-center px-6 max-md:py-3 md:py-6 mx-auto bg-gradient-to-r from-teal-500 to-sky-500 rounded-xl shadow">
+      <i class="ri-roadster-line text-2xl"></i>
+      <p class="text-lg font-semibold md:py-2 max-md:py-1">Désignez votre voiture</p>
+      <p class="mlgtext-md max-lg:text-sm md:py-3 max-md:py-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Condimentum vitae sapien pellentesque habitant morbi tristique senectus.</p>
+    </div>
+    <!-- Payment -->
+    <div
+      class="md:w-full max-md:w-80 text-center px-6 max-md:py-3 md:py-6 mx-auto bg-gradient-to-r from-teal-500 to-sky-500 rounded-xl shadow">
+      <i class="ri-bank-card-line text-2xl"></i>
+      <p class="text-lg font-semibold py-2">Moyens diverses de payement</p>
+      <p class="lg:text-md max-lg:text-sm py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+        Condimentum vitae sapien pellentesque habitant morbi tristique senectus.</p>
+    </div>
+  </div>
+</div>
 
 
 
--- resources/views/livewire/form-accueil.blade.php
+++ resources/views/livewire/form-accueil.blade.php
@@ -6,86 +6,109 @@
             <div class="flex flex-row max-sm:flex-col w-full max-lg:gap-2">
                 {{-- Lieu de Départ --}}
                 <label for="lieuDepart" class="w-full" x-data="{ lieuDepart:''}">
-            
-                    <input name="lieuDepart" type="text" placeholder="Lieu de départ" wire:model.live.debounce.100ms="lieuDepart" autocomplete="off" x-model="lieuDepart"
-                            class="relative w-full p-3 cursor-pointer focus:ring-0 border-2 border-slate-300 focus:border-teal-500 max-lg:rounded-lg lg:rounded-l-xl">
+
+                    <input name="lieuDepart" type="text" placeholder="Lieu de départ"
+                        wire:model.live.debounce.100ms="lieuDepart" autocomplete="off" x-model="lieuDepart"
+                        class="relative w-full p-3 cursor-pointer focus:ring-0 border-2 border-slate-300 focus:border-teal-500 max-lg:rounded-lg lg:rounded-l-xl">
                     @if ( count($lieuxDepart) > 0 )
-                        <div class="absolute z-10 mt-1 w-80 bg-white rounded shadow-xl p-2 font-cabin">
-                            <div wire:loading wire:target="lieuDepart" class="p-2"><p class="text-sm">Chargement...</p></div>
-                                @if( $lieuxDepart->where('type','Aéroport')->isNotEmpty())
-                                    <p wire:loading.remove wire:target="lieuDepart" class=" text-teal-600 underline text-sm">Aéroports : </p>
-                                    <div wire:loading.remove wire:target="lieuDepart">
-                                        @foreach ($lieuxDepart->where('type','Aéroport') as $lieu)
-                                            <p wire:loading.remove wire:click="appliquerLieuDepart('{{ $lieu->nom }}','{{ $lieu->id }}')" x-on:click="lieuDepart='{{ $lieu->nom }}'"
-                                                class="truncate px-2 py-2 cursor-pointer hover:bg-teal-100">
-                                                <i class="font-normal ri-plane-line text-xl text-teal-500 mr-3"></i>{{ $lieu->nom }}</p>
-                                        @endforeach
-                                    </div>
-                                @endif
-                                @if( $lieuxDepart->where('type','Gare Routière')->isNotEmpty())
-                                    <p wire:loading.remove wire:target="lieuDepart" class=" text-teal-600 underline text-sm">Stations : </p>
-                                    <div wire:loading.remove wire:target="lieuDepart">
-                                        @foreach ($lieuxDepart->where('type','Gare Routière') as $lieu)
-                                            <p  wire:click="appliquerLieuDepart('{{ $lieu->nom }}','{{ $lieu->id }}')" x-on:click="lieuDepart='{{ $lieu->nom }}'"
-                                                class="truncate px-2 py-2 cursor-pointer hover:bg-teal-100">
-                                                <i class="font-normal ri-bus-fill text-xl text-teal-500 mr-3"></i>{{ $lieu->nom }}</p>
-                                        @endforeach
-                                    </div>
-                                @endif
+                    <div class="absolute z-30 mt-1 w-80 bg-white rounded shadow-xl p-2 font-cabin">
+                        <div wire:loading wire:target="lieuDepart" class="p-2">
+                            <p class="text-sm">Chargement...</p>
                         </div>
+                        @if( $lieuxDepart->where('type','Aéroport')->isNotEmpty())
+                        <p wire:loading.remove wire:target="lieuDepart" class=" text-teal-600 underline text-sm">
+                            Aéroports : </p>
+                        <div wire:loading.remove wire:target="lieuDepart">
+                            @foreach ($lieuxDepart->where('type','Aéroport') as $lieu)
+                            <p wire:loading.remove
+                                wire:click="appliquerLieuDepart('{{ $lieu->nom }}','{{ $lieu->id }}')"
+                                x-on:click="lieuDepart='{{ $lieu->nom }}'"
+                                class="truncate px-2 py-2 cursor-pointer hover:bg-teal-100">
+                                <i class="font-normal ri-plane-line text-xl text-teal-500 mr-3"></i>{{ $lieu->nom }}
+                            </p>
+                            @endforeach
+                        </div>
+                        @endif
+                        @if( $lieuxDepart->where('type','Gare Routière')->isNotEmpty())
+                        <p wire:loading.remove wire:target="lieuDepart" class=" text-teal-600 underline text-sm">
+                            Stations : </p>
+                        <div wire:loading.remove wire:target="lieuDepart">
+                            @foreach ($lieuxDepart->where('type','Gare Routière') as $lieu)
+                            <p wire:click="appliquerLieuDepart('{{ $lieu->nom }}','{{ $lieu->id }}')"
+                                x-on:click="lieuDepart='{{ $lieu->nom }}'"
+                                class="truncate px-2 py-2 cursor-pointer hover:bg-teal-100">
+                                <i class="font-normal ri-bus-fill text-xl text-teal-500 mr-3"></i>{{ $lieu->nom }}
+                            </p>
+                            @endforeach
+                        </div>
+                        @endif
+                    </div>
                     @endif
-            
+
                     @error('lieuDepart')<p class="text-red-600 text-sm p-2">{{ $message }}</p>@enderror
                 </label>
                 {{-- Lieu de Retour --}}
                 <label for="lieuRetour" class="w-full" x-data="{ lieuRetour:''}">
-            
-                    <input name="lieuRetour" type="text" placeholder="Lieu de retour" wire:model.live.debounce.100ms="lieuRetour" autocomplete="off" x-model="lieuRetour"
-                            class="relative w-full p-3 cursor-pointer focus:ring-0 border-2 border-slate-300 focus:border-teal-500 max-lg:rounded-lg lg:border-l-0">
-                            
+
+                    <input name="lieuRetour" type="text" placeholder="Lieu de retour"
+                        wire:model.live.debounce.100ms="lieuRetour" autocomplete="off" x-model="lieuRetour"
+                        class="relative w-full p-3 cursor-pointer focus:ring-0 border-2 border-slate-300 focus:border-teal-500 max-lg:rounded-lg lg:border-l-0">
+
                     @if ( count($lieuxRetour) > 0 )
-                        <div class="absolute z-10 w-80 mt-1 bg-white rounded shadow-md p-2 font-cabin">
-            
-                            <div wire:loading wire:target="lieuRetour" class="p-2"><p class="text-sm">Chargement...</p></div>
-            
-                                @if( $lieuxRetour->where('type','Aéroport')->isNotEmpty())
-                                    <p wire:loading.remove wire:target="lieuRetour" class=" text-teal-600 underline text-sm">Aéroports :</p>
-                                    <div wire:loading.remove wire:target="lieuRetour">
-                                        @foreach ($lieuxRetour->where('type','Aéroport') as $lieu)
-                                            <p wire:loading.remove wire:click="appliquerLieuRetour('{{ $lieu->nom }}','{{ $lieu->id }}')" x-on:click="lieuRetour='{{ $lieu->nom }}'"
-                                                class="truncate px-4 py-2 cursor-pointer hover:bg-teal-100">
-                                                <i class="font-normal ri-plane-line text-xl text-teal-500 mr-3"></i>{{ $lieu->nom }}</p>
-                                        @endforeach
-                                    </div>
-                                @endif
-                                @if( $lieuxRetour->where('type','Gare Routière')->isNotEmpty())
-                                    <p wire:loading.remove wire:target="lieuRetour" class=" text-teal-600 underline text-sm ">Stations : </p>
-                                    <div wire:loading.remove wire:target="lieuRetour">
-                                        @foreach ($lieuxRetour->where('type','Gare Routière') as $lieu)
-                                            <p wire:loading.remove wire:click="appliquerLieuRetour('{{ $lieu->nom }}','{{ $lieu->id }}')" x-on:click="lieuRetour='{{ $lieu->nom }}'"
-                                                class="truncate px-4 py-2 cursor-pointer hover:bg-teal-100">
-                                                <i class="font-normal ri-bus-fill text-xl text-teal-500 mr-3"></i>{{ $lieu->nom }}</p>
-                                        @endforeach
-                                    </div>
-                                @endif
-            
+                    <div class="absolute z-30 w-80 mt-1 bg-white rounded shadow-md p-2 font-cabin">
+
+                        <div wire:loading wire:target="lieuRetour" class="p-2">
+                            <p class="text-sm">Chargement...</p>
                         </div>
+
+                        @if( $lieuxRetour->where('type','Aéroport')->isNotEmpty())
+                        <p wire:loading.remove wire:target="lieuRetour" class=" text-teal-600 underline text-sm">
+                            Aéroports :</p>
+                        <div wire:loading.remove wire:target="lieuRetour">
+                            @foreach ($lieuxRetour->where('type','Aéroport') as $lieu)
+                            <p wire:loading.remove
+                                wire:click="appliquerLieuRetour('{{ $lieu->nom }}','{{ $lieu->id }}')"
+                                x-on:click="lieuRetour='{{ $lieu->nom }}'"
+                                class="truncate px-4 py-2 cursor-pointer hover:bg-teal-100">
+                                <i class="font-normal ri-plane-line text-xl text-teal-500 mr-3"></i>{{ $lieu->nom }}
+                            </p>
+                            @endforeach
+                        </div>
+                        @endif
+                        @if( $lieuxRetour->where('type','Gare Routière')->isNotEmpty())
+                        <p wire:loading.remove wire:target="lieuRetour" class=" text-teal-600 underline text-sm ">
+                            Stations : </p>
+                        <div wire:loading.remove wire:target="lieuRetour">
+                            @foreach ($lieuxRetour->where('type','Gare Routière') as $lieu)
+                            <p wire:loading.remove
+                                wire:click="appliquerLieuRetour('{{ $lieu->nom }}','{{ $lieu->id }}')"
+                                x-on:click="lieuRetour='{{ $lieu->nom }}'"
+                                class="truncate px-4 py-2 cursor-pointer hover:bg-teal-100">
+                                <i class="font-normal ri-bus-fill text-xl text-teal-500 mr-3"></i>{{ $lieu->nom }}
+                            </p>
+                            @endforeach
+                        </div>
+                        @endif
+
+                    </div>
                     @endif
-            
+
                     @error('lieuRetour') <p class="text-red-600 text-sm p-2">{{ $message }}</p>@enderror
                 </label>
             </div>
             <div class="flex flex-row max-sm:flex-col w-full max-lg:gap-2">
                 {{-- Date de Départ --}}
                 <label for="dateDepart" class="w-full">
-                    <input id="dateTime1" name="dateDepart" type="text" placeholder="Date de départ" wire:model.blur="dateDepart"
-                    class="p-3 w-full cursor-pointer focus:ring-0 border-2 border-slate-300 focus:border-teal-500 max-lg:rounded-lg lg:border-l-0">
+                    <input id="dateTime1" name="dateDepart" type="text" placeholder="Date de départ"
+                        wire:model.blur="dateDepart"
+                        class="p-3 w-full cursor-pointer focus:ring-0 border-2 border-slate-300 focus:border-teal-500 max-lg:rounded-lg lg:border-l-0">
                     @error('dateDepart') <p class="text-red-600 text-sm p-2">{{ $message }}</p> @enderror
                 </label>
                 {{-- Date de Retour --}}
                 <label for="dateRetour" class="w-full">
-                    <input id="dateTime2" name="dateRetour" type="text" placeholder="Date de retour" wire:model.blur="dateRetour"
-                    class="p-3 w-full cursor-pointer focus:ring-0 border-2 border-slate-300 focus:border-teal-500 max-lg:rounded-lg lg:border-l-0 lg:rounded-r-xl">
+                    <input id="dateTime2" name="dateRetour" type="text" placeholder="Date de retour"
+                        wire:model.blur="dateRetour"
+                        class="p-3 w-full cursor-pointer focus:ring-0 border-2 border-slate-300 focus:border-teal-500 max-lg:rounded-lg lg:border-l-0 lg:rounded-r-xl">
                     @error('dateRetour') <p class="text-red-600 text-sm p-2">{{ $message }}</p> @enderror
                 </label>
             </div>
@@ -93,23 +116,19 @@
         </div>
         <div class="flex flex-row align-middle justify-between pt-2 text-sm mx-2">
             <div class="flex flex-row gap-2 place-items-center">
-              <p>Mon age :</p>
-              <select name="minAge" wire:model.blur="minAge" class="border-none focus:border-none">
-                @for($i = 18; $i <= 25; $i++)
-                    <option value="{{ $i }}">{{ $i }}</option>
-                @endfor
-                <option value="26">26+</option>
-              </select>
-              @error('minAge') <p class="text-red-600 text-sm p-2">{{ $message }}</p> @enderror
+                <p>Mon age :</p>
+                <select name="minAge" wire:model.blur="minAge" class="border-none focus:border-none">
+                    @for($i = 18; $i <= 25; $i++) <option value="{{ $i }}">{{ $i }}</option>
+                        @endfor
+                        <option value="26">26+</option>
+                </select>
+                @error('minAge') <p class="text-red-600 text-sm p-2">{{ $message }}</p> @enderror
             </div>
-            <button type="submit" class="px-5 py-2 bg-teal-500 hover:bg-teal-600 text-white cursor-pointer font-semibold rounded-lg shadow-md uppercase transition-all">
+            <button type="submit"
+                class="px-5 py-2 bg-teal-500 hover:bg-teal-600 text-white cursor-pointer font-semibold rounded-lg shadow-md uppercase transition-all">
                 Rechercher
             </button>
         </div>
-    </form> 
+    </form>
 
 </div>
-
-
-
-
